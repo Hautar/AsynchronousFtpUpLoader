f06f1b72c3f46541dcb043d08ff5732ee8b6e2a4
Hautar <58347348+Hautar@users.noreply.github.com>
2022-04-20 16:52:41 +0300

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,26 @@
+﻿namespace FtpGazApp
+{
+    public class Settings
+    {
+        public string UserName;
+        public string Password;
+        
+        public string HostUrl;
+        public string RemotePath;
+
+        public string LocalPath;
+        public string LocalPathUploaded;
+
+        public Settings()
+        {
+            UserName = default;
+            Password = default;
+
+            HostUrl = default;
+            RemotePath = default;
+
+            LocalPath = default;
+            LocalPathUploaded = default;
+        }
+    }
+}
